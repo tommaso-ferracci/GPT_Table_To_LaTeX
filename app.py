5c96c9039cdc03ce4551f0c21bb5419b3676bc7f
Tommaso Ferracci <tommaso.ferracci@gmail.com>
2023-08-03 19:01:54 +0200

    initialize repository

---
+++ app.py
@@ -0,0 +1,28 @@
+import streamlit as st
+from io import StringIO
+from utils import get_completion
+from utils import convert_to_latex
+from streamlit_extras.buy_me_a_coffee import button
+
+st.title("Convert CSV to $\LaTeX$ with ChatGPT")
+
+st.write("Please upload your CSV file below.")
+
+uploaded_file = st.file_uploader("Upload a CSV")
+
+if uploaded_file is not None:
+
+    data = StringIO(uploaded_file.getvalue().decode("utf-8")).read()
+
+    st.write("Converting... this might take a minute...")
+
+    result = convert_to_latex(data)
+
+    if type(result) != str:
+        st.write(f"Table is too long. GPT-3.5-turbo supports up to 4096 tokens, while your prompt requires {result} tokens.")
+    else:
+        st.code(result, language='latex')
+
+        st.write("*Using the GPT-3.5-turbo API costs money. If you are satisfied with the result and want to help keep the project free, consider donating a coffee:*")
+
+        button(username="risiandbisi", floating=False, width=221)
---
+++ utils.py
@@ -0,0 +1,37 @@
+import openai
+import tiktoken
+import os
+
+from dotenv import load_dotenv, find_dotenv
+_ = load_dotenv(find_dotenv()) # read local .env file
+
+openai.api_key = os.environ['OPENAI_API_KEY']
+
+def get_completion(prompt, model="gpt-3.5-turbo", temperature=0): 
+    messages = [{"role": "user", "content": prompt}]
+    response = openai.ChatCompletion.create(
+        model=model,
+        messages=messages,
+        temperature=temperature, 
+    )
+    return response.choices[0].message["content"]
+
+def convert_to_latex(data):
+
+    delimiter = "####"
+
+    prompt = f"""
+    Your task is to convert the following text, delimited by {delimiter}, into a LaTeX table. 
+    Return only the LaTeX code needed for the table. By default, round all floats to 2 decimal places unless the start with 0.00, in which case round them 
+    to the first non-null digit. Leave integers as they are. By default, center and separate all columns. By default, use \hline at the beginning and end of
+    the table. By default, use \hline after the header. Remember to close all environments.
+    {delimiter}{data}{delimiter} 
+    """
+
+    tokenizer = tiktoken.encoding_for_model("gpt-3.5-turbo")
+    num_tokens = len(tokenizer.encode(prompt))
+
+    if num_tokens < 4096:
+        return get_completion(prompt)
+    else:
+        return num_tokens
