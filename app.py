cc7911821f90f2f0797d3952de2043ffab576562
Tommaso Ferracci <tommaso.ferracci@gmail.com>
2023-08-12 11:58:46 +0200

    update app to support user input

--- app.py
+++ app.py
@@ -1,31 +1,55 @@
 import os
 import streamlit as st
 from io import StringIO
+from rebuff import Rebuff
 from utils import get_completion
+from utils import count_tokens
 from utils import convert_to_latex
 from streamlit_extras.buy_me_a_coffee import button
 
 os.environ['OPENAI_API_KEY'] = st.secrets['OPENAI_API_KEY']
-
+rb = Rebuff(api_token=st.secrets['REBUFF_API_KEY'], api_url="https://alpha.rebuff.ai")
 st.title("Convert CSV to $\LaTeX$ with ChatGPT")
-
 st.write("Please upload your CSV file below.")
-
-uploaded_file = st.file_uploader("Upload a CSV")
+uploaded_file = st.file_uploader("", label_visibility="collapsed")
 
 if uploaded_file is not None:
-
     data = StringIO(uploaded_file.getvalue().decode("utf-8")).read()
+    delimiter = "####"
+    context = []
+    system_message = f"""
+    Your task is to convert the following text, delimited by {delimiter}, into a LaTeX table. 
+    Return only the LaTeX code needed for the table. By default, round all floats to 2 decimal places unless the start with 0.00, in which case round them 
+    to the first non-null digit. By default, leave integers as they are. By default, center and separate all columns. By default, use \hline at the beginning and end of
+    the table. By default, use \hline after the header. Remember to close all environments.
+    {delimiter}{data}{delimiter} 
+    The user might prompt you with personal stylistic preferences. Remember to always output only LaTeX code.
+    """
+    context.append({"role": "system", "content": system_message})
+    num_tokens = count_tokens(context)
 
-    st.write("Converting... this might take a minute...")
-
-    result = convert_to_latex(data)
-
-    if type(result) != str:
-        st.write(f"Table is too long. GPT-3.5-turbo supports up to 4096 tokens, while your prompt requires {result} tokens.")
+    if num_tokens > 4096:
+        st.write(f"Table is too long. GPT-3.5-turbo supports up to 4096 tokens, while your prompt requires {num_tokens} tokens.")
     else:
+        st.write("Converting... this might take a minute...")
+        result = convert_to_latex(context)
         st.code(result, language='latex')
 
-        st.write("*Using the GPT-3.5-turbo API costs money. If you are satisfied with the result and want to help keep the project free, consider donating a coffee:*")
+        for i in range(10):
+            satisfaction = st.radio("Are you satisfied with the result?", ('Yes', 'No'))
 
-        button(username="risiandbisi", floating=False, width=221)
+            if satisfaction == 'Yes':
+                st.write("*Using the GPT-3.5-turbo API costs money. If you want to help keep the project free, consider donating a coffee:*")
+                button(username='risiandbisi', floating=False, width=220)
+                break
+            else: 
+                user_input = st.text_input("Please provide specific stylistic preferences:", "")
+                _, is_injection = rb.detect_injection(user_input)
+
+                while is_injection:
+                    user_input = st.text_input("It looks like you are attempting a prompt injection. Please provide only specific stylistic preferences:", "")
+                    _, is_injection = rb.detect_injection(user_input)
+
+                result = convert_to_latex(context, user_input)
+                st.code(result, language='latex')
+                i += 1
--- utils.py
+++ utils.py
@@ -1,8 +1,7 @@
 import openai
 import tiktoken
 
-def get_completion(prompt, model="gpt-3.5-turbo", temperature=0): 
-    messages = [{"role": "user", "content": prompt}]
+def get_completion(messages, model="gpt-3.5-turbo", temperature=0):
     response = openai.ChatCompletion.create(
         model=model,
         messages=messages,
@@ -10,22 +9,16 @@ def get_completion(prompt, model="gpt-3.5-turbo", temperature=0):
     )
     return response.choices[0].message["content"]
 
-def convert_to_latex(data):
-
-    delimiter = "####"
-
-    prompt = f"""
-    Your task is to convert the following text, delimited by {delimiter}, into a LaTeX table. 
-    Return only the LaTeX code needed for the table. By default, round all floats to 2 decimal places unless the start with 0.00, in which case round them 
-    to the first non-null digit. Leave integers as they are. By default, center and separate all columns. By default, use \hline at the beginning and end of
-    the table. By default, use \hline after the header. Remember to close all environments.
-    {delimiter}{data}{delimiter} 
-    """
-
+def count_tokens(messages):
     tokenizer = tiktoken.encoding_for_model("gpt-3.5-turbo")
-    num_tokens = len(tokenizer.encode(prompt))
+    num_tokens = len(tokenizer.encode(messages[0]["content"]))
+    return num_tokens
 
-    if num_tokens < 4096:
-        return get_completion(prompt)
+def convert_to_latex(messages, user_input=None):
+    if user_input == None:
+        return get_completion(messages)
     else:
-        return num_tokens
+        messages.append({"role": "user", "content": user_input})
+        response = get_completion(messages)
+        messages.append({"role": "assistant", "content": response})
+        return response
